840124c82e836142ca7f3c0774f0adcd3847e9e4
Jami Tefridj <jamitefridj@gmail.com>
2019-03-10 22:44:13 -0500

    adding all updated script email lambda functions

---
+++ auroraCardEmail.py
@@ -0,0 +1,47 @@
+# import smtplib, ssl
+
+# sender_email = "auroracardemailtest@gmail.com"
+# receiver_email = "spencermaxwell96@gmail.com" # will need to be parameter for incoming email
+# message = """\
+#     Subject: Hi Test
+
+
+#     This is my first test."""
+
+# port = 465  #Required for SSL
+# password = input("Type your password and press enter: ")
+
+# # Create a secure SSL context
+# context = ssl.create_default_context()
+
+# with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+#     server.login(sender_email, password)
+#     server.sendmail(sender_email, receiver_email, message)
+
+import yagmail
+
+#receiver = "spencermaxwell96@gmail.com"
+body = """\
+Greetings,
+
+Thank you for your interest in using our augmented reality cards. You will find a printout for three cards attached to this email. Simply print out the cards and use the app to play AR games right from your phone. 
+
+We will let you know when more cards become available. 
+
+Enjoy!
+Aurora Team"""
+filename="Aurora_Card_Printout.pdf"
+
+def handler(event, context):
+    email = event['email']
+    source = event['source']
+    
+    yagmail.register("auroracardemailtest@gmail.com", "actest123")
+    
+    yag = yagmail.SMTP("auroracardemailtest@gmail.com")
+    yag.send(
+        to=email,
+        subject="Here are your free Aurora Cards!",
+        contents=body,
+        attachments=filename,
+    )
---
+++ auroraCardEmailInsert.py
@@ -0,0 +1,35 @@
+import sys
+import logging
+import rds_config
+import pymysql
+#rds settings
+rds_host  = "auroradbmysql.crmqojacvx6b.us-east-2.rds.amazonaws.com"
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+
+def handler(event, context):
+    email = event['email']
+    source = event['source']
+    email_source_dict = {"email": email, "source": source}
+    """
+    This function fetches content from MySQL RDS instance
+    """
+    cursor = conn.cursor()
+
+    sql_insert_location = """insert into cardEmail set email=%(email)s, source=%(source)s"""
+
+    cursor.execute(sql_insert_location, email_source_dict)
+
+    return cursor._last_executed
